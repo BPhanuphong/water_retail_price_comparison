ca201b46df13aaaeb218f9ac7ed4a9aa6fa29ae5
BPhanuphong <113247700+BPhanuphong@users.noreply.github.com>
2024-04-26 14:51:20 +0700

    Update scrape_water_lotus.py

--- code/scrape_water_lotus.py
+++ code/scrape_water_lotus.py
@@ -5,20 +5,27 @@ from bs4 import BeautifulSoup
 
 def main_scraping():
     print('Start Scraping')
-    
+    # Lotus API
     url = "https://api-o2o.lotuss.com/lotuss-mobile-bff/product/v2/products?q=%7B%22offset%22:{0},%22limit%22:100,%22filter%22:%7B%22categoryId%22:[%2291288%22]%7D,%22websiteCode%22:%22thailand_hy%22%7D"
 
+    # Headers is required 'Accept-Language' , Select 'en' or 'th'
     payload={}
     headers = {
     'Accept-Language': 'th'
     }
-
+    
+    # Making requests to API using Get Method
     response = requests.request("GET", url.format(0), headers=headers, data=payload)
+
+    #Get Respong as json
     response_json = eval(response.text.replace('null','None').replace('false','False').replace('true','True'))
 
+    # Find number of page
     no_page = response_json['meta']['total']//response_json['meta']['limit']+1
     product_name = []
     price = []
+
+    # Loop through each page
     for page in range(0,no_page):
         print('Getting page {0}'.format(page))
         if page>0 :
@@ -30,8 +37,10 @@ def main_scraping():
             
             response = requests.request("GET", url.format(page*100), headers=headers, data=payload)
             response_json = eval(response.text.replace('null','None').replace('false','False').replace('true','True'))
-
+            
+        # Extract product name
         [product_name.append(response_json['data']['products'][item]['name']) for item in range(len(response_json['data']['products']))]
+        # Extract product price
         [price.append(response_json['data']['products'][item]['priceRange']['minimumPrice']['finalPrice']['value']) for item in range(len(response_json['data']['products']))]
 
     lotus_df = pd.DataFrame(data={'product_name':product_name,

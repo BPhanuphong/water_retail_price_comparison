7491144593f18a507423bc05492ca00694fff4f5
BPhanuphong <113247700+BPhanuphong@users.noreply.github.com>
2024-04-26 15:26:51 +0700

    Update matching.py

--- code/matching.py
+++ code/matching.py
@@ -3,33 +3,37 @@ import time
 import editdistance
 import re
 
-
+# Use for extract size from product name
 def get_size(name):
-# for name in bigC_df['product_name']:
-    name = name.replace(' ','').replace('มิลลิลิตร','มล.').replace('ลิตร','ล.').replace(',','')
+    name = name.replace(' ','').replace('มิลลิลิตร','มล.').replace('ลิตร','ล.').replace(',','')     # Change to same format of mesure
+
+    # Find number in front of ล,มล,ล.,มล.
     pattern = r'\d+(\.\d+)?ล\.|\d+(\.\d+)?ล|\d+(\.\d+)?มล\.|\d+(\.\d+)?มล'
     if re.search(pattern, name):
         name_match = re.search(pattern, name)[0]
         if 'มล' not in name_match:
-            return int(float(name_match.split('ล')[0])*1000)
+            return int(float(name_match.split('ล')[0])*1000) # Convert to milliliter
         else:
             return int(name_match.split('มล')[0])
     else: 
         return None
 
-
+# Use for extract quantity from product name
 def get_quantity (name):
-# for name in bigC_df['product_name']:
+
     name = name.replace(' ','')
+    
+    # Find number in after of แพ็ค,X or in front of ขวด
     pattern = r'แพ็ค\s*(\d+)|(\d+\s*ขวด)|x\s*(\d+)|X\s*(\d+)'
     if re.search(pattern, name):
         name_match = re.search(pattern, name)
-        return int(name_match[0].replace('แพ็ค','').replace('ขวด','').replace('x','').replace('X',''))
+        return int(name_match[0].replace('แพ็ค','').replace('ขวด','').replace('x','').replace('X',''))  #Remove all unit to extract only number
     else:
         return 1
 
 
 def clean_name(name):
+    #Remove Size and Quantity from product name
     name = name.replace(' ','').replace('มิลลิลิตร','มล.').replace('ลิตร','ล.').replace(',','').replace('ชิ้น','')
     size_pattern = r'\d+(\.\d+)?ล\.|\d+(\.\d+)?ล|\d+(\.\d+)?มล\.|\d+(\.\d+)?มล'
     quantity_pattern = r'แพ็ค\s*(\d+)|(\d+\s*ขวด)|x\s*(\d+)|X\s*(\d+)'
@@ -57,20 +61,28 @@ def main():
     lotus_df = lotus_df.reset_index().rename(columns={'index': 'lotus_id'})
     bigC_df = bigC_df.reset_index().rename(columns={'index': 'bigc_id'})
     bigC_df['lotus_id'] = None
+
+    # Check for matching
     for index in range(len(bigC_df)):
         bigc_name = bigC_df['clean_name'].loc[index]
         print(index)
         print('big c:' ,bigc_name)
+
+        # Filter for same size and quantity
         match_size_df = lotus_df[(lotus_df['size']==bigC_df['size'].loc[index])&(lotus_df['quantity']==bigC_df['quantity'].loc[index])]
         name_list = match_size_df['clean_name'].values.tolist()
         id_list =  match_size_df['lotus_id'].values.tolist()
         score = []
         if len(name_list)>0:
+
+            #Loop to check name similarity in name_list
             for name in name_list:
                 distance = editdistance.eval(bigc_name, name)
                 max_length = max(len(bigc_name), len(name))
                 similarity = 1 - (distance / max_length)
                 score.append(similarity)
+                
+            # Set score to be above 0.8
             if max(score)>0.8:
                 print('Matching')
                 print(id_list[score.index(max(score))])
